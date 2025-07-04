73e211d6fb737d8cd4c29463d8055690e6dacf7c
Said <said.tecpa.juarez.04@gmail.com>
2025-07-04 02:03:56 -0600

    Edit for.py

--- Python/03_for.py
+++ Python/03_for.py
@@ -3,17 +3,19 @@
 #   Bloque de código a repetir
 #   instrucciones
 
-# frutas = ["manzana", "banana", "naranja"]
-
-# for fruta in frutas:
-#     print(fruta)
-
-# for i in range(5):
-#     pass
-
 for i in range(10):
 
     if i % 2 == 0:
         continue    # Solo es útil en loops
     print(i)
+""" 
+Condición continue
+Si es verdadera (es decir, i es par), se ejecuta continue.
+Esto provoca que el bucle salte inmediatamente al siguiente valor
+de i, sin ejecutar las líneas que vienen después dengtro del cuerpo 
+del for.
+
+Por otra parte, si es falsa (es decir, i es impar), no entra en el if
+y continúa ejecutando la siguiente instrucción.
+ """
 
