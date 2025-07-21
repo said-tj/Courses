14419997a0aef6bb365c7e576d0dacf236abf4c6
Said <said.tecpa.juarez.04@gmail.com>
2025-07-21 14:57:48 -0600

    New script

---
+++ Python/04_1_tuplas.py
@@ -0,0 +1,15 @@
+"""
+Una tupla es una estructura de datos inmutable y ordenada que 
+permite almacenar una colección de elementos. Los elementos 
+de una tupla se encierran entre paréntesis (), separados por
+comas.
+"""
+punto = (3, 4)
+
+"""
+Para acceder a los elementos de una tupla, utiliza el índice 
+del elemento entre corchetes, similar a las listas.
+"""
+
+print(punto[0])
+print(punto[1])
